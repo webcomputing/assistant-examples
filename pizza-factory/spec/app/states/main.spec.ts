f1ecd031d4dc9403778403e3533a391261d3c75f
PiCookie <l.erfkaemper@gmx.de>
2018-09-24 05:30:25 -0700

    finished main spec

--- pizza-factory/spec/app/states/main.spec.ts
+++ pizza-factory/spec/app/states/main.spec.ts
@@ -1,64 +1,90 @@
-import { GenericIntent, injectionNames, Session } from "assistant-source";
+import { MainState } from "../../../app/states/main";
 import { ThisContext } from "../../support/this-context";
 
 interface CurrentThisContext extends ThisContext {
-  currentSessionFactory: () => Session;
-  currentStateNameProvider: () => Promise<string>;
+  currentState: MainState;
 }
 
 describe("MainState", function() {
   describe("on platform = alexa", function() {
+    beforeEach(async function(this: CurrentThisContext) {
+      this.fixMathSeed();
+    });
+
     describe("invokeGenericIntent", function() {
-      beforeEach(async function(this: CurrentThisContext) {
-        await this.callIntent(GenericIntent.Invoke);
+      describe("test amount of pizzas", function() {
+        beforeEach(async function(this: CurrentThisContext) {
+          await this.prepareCurrentStateForTest("MainState", "invokeGenericIntent");
+          this.currentState = this.grabState<MainState>(await this.getCurrentStateName());
+          await this.currentState.sessionFactory().set("amountOfPizzas", "1");
+          await this.runMachineAndGetResults("MainState");
+        });
+
+        it("stores amount of pizzas into session", async function(this: CurrentThisContext) {
+          const amountOfPizzas = (await this.currentState.sessionFactory().get("amountOfPizzas")) || 0;
+          expect(+amountOfPizzas).toBe(1);
+          expect(+amountOfPizzas).not.toBe(0);
+        });
       });
 
-      it("stores amount of pizzas into session", async function(this: CurrentThisContext) {
-        this.currentSessionFactory = this.container.inversifyInstance.get(injectionNames.current.sessionFactory);
-        const amountOfPizzas = (await this.currentSessionFactory().get("amountOfPizzas")) || 0;
-        expect(+amountOfPizzas).toBe(1);
-        expect(+amountOfPizzas).not.toBe(0);
-      });
+      describe("prompt", function() {
+        beforeEach(async function(this: CurrentThisContext) {
+          await this.prepareCurrentStateForTest("MainState", "invokeGenericIntent");
+          await this.runMachineAndGetResults("MainState");
+        });
 
-      it("greets and prompts for command", async function(this: CurrentThisContext) {
-        expect(await this.translateValuesFor()("mainState.invokeGenericIntent.alexa")).toContain(this.responseHandlerResults.voiceMessage!.text);
+        it("greets and prompts for command", async function(this: CurrentThisContext) {
+          expect(await this.responseHandlerResults.voiceMessage!.text).toContain((await this.translateValuesFor()("mainState.invokeGenericIntent.alexa"))[0]);
+        });
       });
     });
 
     describe("getToppingsIntent", function() {
+      beforeEach(async function(this: CurrentThisContext) {
+        await this.prepareCurrentStateForTest("MainState", "getToppingsIntent");
+        await this.runMachineAndGetResults("MainState");
+      });
+
       it("lists all available toppings", async function(this: CurrentThisContext) {
-        await this.callIntent("getToppings");
-        expect(await this.translateValuesFor()("mainState.getToppingsIntent")).toContain(this.responseHandlerResults.voiceMessage!.text);
+        expect(await this.responseHandlerResults.voiceMessage!.text).toContain((await this.translateValuesFor()("mainState.getToppingsIntent"))[0]);
       });
     });
 
     describe("orderPizzaIntent", function() {
       beforeEach(async function(this: CurrentThisContext) {
-        await this.callIntent("orderPizza");
+        await this.prepareCurrentStateForTest("MainState", "orderPizzaIntent");
+        await this.runMachineAndGetResults("MainState");
       });
 
       it("order pizza", async function(this: CurrentThisContext) {
-        expect(await this.translateValuesFor()("mainState.orderPizzaIntent")).toContain(this.responseHandlerResults.voiceMessage!.text);
+        expect(await this.responseHandlerResults.voiceMessage!.text).toContain((await this.translateValuesFor()("mainState.orderPizzaIntent"))[0]);
       });
 
-      it("sets the current conversation state to 'PizzaState'", async function(this: CurrentThisContext) {
-        this.currentStateNameProvider = this.container.inversifyInstance.get(injectionNames.current.stateNameProvider);
-        const currentStateName = await this.currentStateNameProvider();
-        expect(currentStateName).toEqual("PizzaState");
+      it("transit to PizzaState", async function() {
+        const state = await this.getCurrentStateName();
+        expect(state).toEqual("PizzaState");
       });
     });
 
     describe("helpGenericIntent", function() {
+      beforeEach(async function(this: CurrentThisContext) {
+        await this.prepareCurrentStateForTest("MainState", "helpGenericIntent");
+        await this.runMachineAndGetResults("MainState");
+      });
+
       it("tries to help", async function(this: CurrentThisContext) {
-        await this.callIntent(GenericIntent.Help);
-        expect(await this.translateValuesFor()("mainState.helpGenericIntent")).toContain(this.responseHandlerResults.voiceMessage!.text);
+        expect(await this.responseHandlerResults.voiceMessage!.text).toContain((await this.translateValuesFor()("mainState.helpGenericIntent"))[0]);
       });
     });
 
     describe("cancelGenericIntent", function() {
+      beforeEach(async function(this: CurrentThisContext) {
+        await this.prepareCurrentStateForTest("MainState", "cancelGenericIntent");
+        await this.runMachineAndGetResults("MainState");
+      });
+
       it("says generic goodbye and ends session", async function(this: CurrentThisContext) {
-        await this.callIntent(GenericIntent.Cancel);
-        expect(await this.translateValuesFor()("root.cancelGenericIntent")).toContain(this.responseHandlerResults.voiceMessage!.text);
+        expect(await this.responseHandlerResults.voiceMessage!.text).toContain((await this.translateValuesFor()("root.cancelGenericIntent"))[0]);
       });
     });
   });
